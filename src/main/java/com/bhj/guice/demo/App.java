295666bc60b7d75fa39496c2f9011f1d11e217ec
Brandon Jackson <brandon.jackson@daveramsey.com>
2019-09-20 07:15:55 -0500

    Setup demo

--- src/main/java/com/bhj/guice/demo/App.java
+++ src/main/java/com/bhj/guice/demo/App.java
@@ -1,7 +1,14 @@
 package com.bhj.guice.demo;
 
+import com.bhj.guice.demo.modules.WidgetModule;
+import com.bhj.guice.demo.widgets.Widget;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
 public class App {
     public static void main(String[] args) {
-
+        Injector injector = Guice.createInjector(new WidgetModule());
+        Widget widget = injector.getInstance(Widget.class);
+        widget.start();
     }
 }
